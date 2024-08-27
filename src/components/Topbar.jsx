f438accdd1ea6e40066a1cbd01433d43d01003b0
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-27 11:54:25 +0300

    topbar edit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,8 +3,22 @@ import React from "react";
 export default function Topbar() {
   return (
     <div className="topbar">
-      <h1>Logo Name</h1>
-      <h2>Design & Develop by mehmet emre</h2>
+      <div className="topbar--logo">
+        <img src="/src/assets/logo.svg" alt="logo" />
+        <h2>Logo Name</h2>
+      </div>
+      <div className="topbar--profile">
+        <div className="topbar--profile-box">
+          <img src="/src/assets/profile image.png" alt="profile" />
+          <div className="topbar--profile-title">
+            <span>Emre Kay.</span>
+            <img src="/src/assets/Property 1=dark-4.svg" alt="sort" />
+          </div>
+        </div>
+        <div className="topbar--profile-icon">
+          <img src="/src/assets/Property 1=dark-3.svg" alt="notification" />
+        </div>
+      </div>
     </div>
   );
 }
