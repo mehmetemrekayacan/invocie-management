8558a9278b55885c358997b2e96a43efc2a90abc
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-29 16:42:59 +0300

    add islogged boolen for sign in sig up vs profile box

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Topbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
@@ -20,11 +21,21 @@ export default function Topbar() {
   };
 
   useEffect(() => {
+    const loggedInStatus = localStorage.getItem("isLoggedIn");
+    if (loggedInStatus === "true") {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
     };
   });
+
   return (
     <div className="topbar">
       <div className="topbar--logo">
@@ -33,58 +44,73 @@ export default function Topbar() {
       </div>
       <div className="topbar--profile">
         <div className="topbar--profile-box">
-          <img src="/src/assets/profile image.png" alt="profile" />
-          <div
-            className={`topbar--profile-title ${
-              isDropdownOpen ? "topbar--dropdown-open" : ""
-            }`}
-            ref={dropdownRef}
-            onClick={toggleDropdown}
-          >
-            <span>Emre Kay.</span>
-            <img
-              className="topbar--dropdown-icon"
-              src="/src/assets/dropdown=dark.svg"
-              alt="sort"
-            />
-          </div>
-          {isDropdownOpen && (
-            <div className="topbar--dropdown-menu">
-              <Link
-                to="/profile/profile"
-                className="topbar--dropdown-item"
-                onClick={closeDropdown}
+          {isLoggedIn ? (
+            <>
+              <img src="/src/assets/profile image.png" alt="profile" />
+              <div
+                className={`topbar--profile-title ${
+                  isDropdownOpen ? "topbar--dropdown-open" : ""
+                }`}
+                ref={dropdownRef}
+                onClick={toggleDropdown}
               >
-                Profile
+                <span>Emre Kay.</span>
+                <img
+                  className="topbar--dropdown-icon"
+                  src="/src/assets/dropdown=dark.svg"
+                  alt="sort"
+                />
+              </div>
+              {isDropdownOpen && (
+                <div className="topbar--dropdown-menu">
+                  <Link
+                    to="/profile/profile"
+                    className="topbar--dropdown-item"
+                    onClick={closeDropdown}
+                  >
+                    Profile
+                  </Link>
+                  <Link
+                    to="/profile/settings"
+                    className="topbar--dropdown-item"
+                    onClick={closeDropdown}
+                  >
+                    Settings
+                  </Link>
+                  <Link
+                    to="/profile/darklight"
+                    className="topbar--dropdown-item"
+                    onClick={closeDropdown}
+                  >
+                    Dark/Light
+                  </Link>
+                  <Link
+                    to="/profile/signout"
+                    className="topbar--dropdown-item"
+                    onClick={closeDropdown}
+                  >
+                    Sign out
+                  </Link>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="topbar--sign-box">
+              <Link to="/login" className="topbar--sign-title">
+                Sign In
               </Link>
-              <Link
-                to="/profile/settings"
-                className="topbar--dropdown-item"
-                onClick={closeDropdown}
-              >
-                Settings
-              </Link>
-              <Link
-                to="/profile/darklight"
-                className="topbar--dropdown-item"
-                onClick={closeDropdown}
-              >
-                Dark/Light
-              </Link>
-              <Link
-                to="/profile/signout"
-                className="topbar--dropdown-item"
-                onClick={closeDropdown}
-              >
-                Sign out
+              <Link to="/signup" className="topbar--sign-title">
+                Sign up
               </Link>
             </div>
           )}
         </div>
-        <div className="topbar--profile-icon">
-          <img src="/src/assets/notification=dark.svg" alt="notification" />
-          <div className="topbar--icon-badge"></div>
-        </div>
+        {isLoggedIn && (
+          <div className="topbar--profile-icon">
+            <img src="/src/assets/notification=dark.svg" alt="notification" />
+            <div className="topbar--icon-badge"></div>
+          </div>
+        )}
       </div>
     </div>
   );
