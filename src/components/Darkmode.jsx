716c13e83607bd4db8927e21c3eeabbb6b043bc3
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-30 10:53:51 +0300

    dark mode make

---
+++ src/components/Darkmode.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./comps.css";
+
+export default function Darkmode() {
+  const switchTheme = (e) => {
+    if (e.target.checked) {
+      document.querySelector("body").setAttribute("data-theme", "dark");
+    } else {
+      document.querySelector("body").setAttribute("data-theme", "light");
+    }
+  };
+  return (
+    <div className="darkmode--container">
+      <label className="switch">
+        <input type="checkbox" onChange={switchTheme}></input>
+        <span className="slider round"></span>
+      </label>
+    </div>
+  );
+}
