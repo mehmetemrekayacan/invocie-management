0ef51053875e5dad596294605bb36fdee3c7f767
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-30 10:58:00 +0300

    edit darktheme lcoation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,38 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./pages.css";
+import Darkmode from "../components/Darkmode";
 
 export default function Register() {
   return (
-    <div className="register--container">
-      <h1>Register</h1>
-      <form className="register--form">
-        <label className="register--enter">
-          <p>Name</p>
-          <input type="text" />
-        </label>
-        <label className="register--enter">
-          <p>Surname</p>
-          <input type="text" />
-        </label>
-        <label className="register--enter">
-          <p>E-Mail</p>
-          <input type="text" />
-        </label>
-        <label className="register--enter">
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <label className="register--enter">
-          <p>Password Again</p>
-          <input type="password" />
-        </label>
-        <label className="register--sign-button">
-          <Link to="/">
-            <button type="signup">Sign Up</button>
-          </Link>
-        </label>
-      </form>
-    </div>
+    <>
+      <Darkmode />
+      <div className="register--container">
+        <h1>Register</h1>
+        <form className="register--form">
+          <label className="register--enter">
+            <p>Name</p>
+            <input type="text" />
+          </label>
+          <label className="register--enter">
+            <p>Surname</p>
+            <input type="text" />
+          </label>
+          <label className="register--enter">
+            <p>E-Mail</p>
+            <input type="text" />
+          </label>
+          <label className="register--enter">
+            <p>Password</p>
+            <input type="password" />
+          </label>
+          <label className="register--enter">
+            <p>Password Again</p>
+            <input type="password" />
+          </label>
+          <label className="register--sign-button">
+            <Link to="/">
+              <button type="signup">Sign Up</button>
+            </Link>
+          </label>
+        </form>
+      </div>
+    </>
   );
 }
